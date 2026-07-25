31f7c41a3899d0948584c3ca12fddb9b21f3d231
somyhussam <119122356+somyhussam@users.noreply.github.com>
2026-07-25 17:44:16 +0300

    Update push event handling and notification display

--- sw.js
+++ sw.js
@@ -1,29 +1,28 @@
 self.addEventListener("push", function(event){
 
-    let data = {};
+    console.log("Push Event وصل");
+
+    let data = {
+        title: "إشعار",
+        body: "تم استلام Push"
+    };
 
     if(event.data){
-        data = event.data.json();
+        try{
+            data = event.data.json();
+            console.log("Data:", data);
+        }catch(e){
+            console.log(e);
+        }
     }
 
     event.waitUntil(
-
-        self.registration.showNotification(
-
-            data.title || "إشعار",
-
-            {
-
-                body:data.body || "",
-
-                icon:"icon.png",
-
-                badge:"icon.png"
-
-            }
-
-        )
-
+        self.registration.showNotification(data.title,{
+            body:data.body,
+            icon:"/icon.png",
+            badge:"/icon.png",
+            requireInteraction:true
+        })
     );
 
 });
